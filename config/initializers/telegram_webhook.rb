bd860ecd06c720543a5c33f89f1d8f50388bfe1b
Razmik Kutinava <razmikg1988@gmail.com>
2026-03-02 18:59:12 +0400

    Init: auto-set Telegram webhook on startup

---
+++ config/initializers/telegram_webhook.rb
@@ -0,0 +1,29 @@
+# Ensure Telegram webhook is configured on app boot. This makes the bot
+# registration more resilient when the app is redeployed.
+
+Rails.application.config.after_initialize do
+  token = ENV['TELEGRAM_BOT_TOKEN'] || ENV['TELEGRAM_TOKEN']
+  webhook_url = ENV['TELEGRAM_WEBHOOK_URL'] || "#{Rails.application.routes.url_helpers.root_url.chomp('/')}telegram/webhook"
+
+  if token.present?
+    begin
+      uri = URI("https://api.telegram.org/bot#{token}/setWebhook")
+      params = { url: webhook_url }
+      uri.query = URI.encode_www_form(params)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri)
+      response = http.request(request)
+
+      Rails.logger.info "Telegram webhook set response: #{response.code} #{response.body}"
+      $stdout.puts "[INIT] Telegram webhook set response: #{response.code} #{response.body}"
+    rescue => e
+      Rails.logger.error "Failed to set Telegram webhook: #{e.message}"
+      $stdout.puts "[INIT] Failed to set Telegram webhook: #{e.message}"
+    end
+  else
+    Rails.logger.warn "Telegram token missing, cannot set webhook in initializer"
+    $stdout.puts "[INIT] Telegram token missing, cannot set webhook in initializer"
+  end
+end
